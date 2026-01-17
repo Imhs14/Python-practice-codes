62d6f2baf08d417a58c5c9402e95ca955c923a31
imhs14 <heerashanker0214@gmail.com>
2026-01-17 19:54:51 +0530

    code update

--- discount.py
+++ discount.py
@@ -1,4 +1,4 @@
-age = input("enter your age")
+age = "22"
 student = False
 discount = True
 if (age>='18' or age<='25') and student or discount:
@@ -11,3 +11,11 @@ weekend = True
 if (temperature<20 and not raining) or weekend:
     print("great day to go out")
 
+#user authentication 
+username = "im_hs14"
+password = "heera123"
+is_verified = True
+if username and password and is_verified:
+    print('login successful')
+else:
+    print("invalid user or password")
