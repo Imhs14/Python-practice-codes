721891ace7c2b4b49ac5fb4b4d605463c1181770
imhs14 <heerashanker0214@gmail.com>
2026-01-19 00:24:08 +0530

    commit

---
+++ For-Loops.py
@@ -0,0 +1,14 @@
+a = ["heera","shanker","malathkar","name"]
+for x in a:
+    print(x)
+
+# we can loop through even a string as they have a sequence of indexing
+for x in "heera":
+    print(x) 
+
+# using the break in the for loop
+fruits = ["apple","banana","cherry","watermelon","orange"]
+for x in fruits:
+    print(x)
+    if x == "watermelon":
+        break
