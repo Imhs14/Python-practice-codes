762f268b73afa09013011b400771d2b952c53cc6
imhs14 <heerashanker0214@gmail.com>
2026-01-22 14:25:13 +0530

    commit

--- args-&-kwargs.py
+++ args-&-kwargs.py
@@ -1,5 +1,16 @@
-def func4(fruits):
-    for fruit in fruits:
-        print(fruit)
-func4(my_fruits = ["apple","mango","pineapple",])
-func4(your_fruits = ["apple","mango","pineapple",])
+# Arbitrary Arguments *args
+def fam(*kids):
+    print("the youngest child is",kids[0])
+
+fam("bob","danny","tony","thor") #out of these 4 the specified kids[0] will be printed according to the postion
+#the youngest child is bob
+
+# *args is a keyword that allows to accept any number of positional arguments
+# accessing the individual arguments
+def acc(*args):
+    print("Type:",type(args))
+    print("first argument:",args[0])
+    print("second argument:",args[1])
+    print("third argument:",args[2])
+    print("all arguments:",args) # you can either write just args or use string slicer [0:3]. both works just fine
+acc("tony","stark","thor","captain")
