0f222b4aaf36d2d739ef6a10c00b44321b5bd912
imhs14 <heerashanker0214@gmail.com>
2026-01-24 18:49:29 +0530

    commit

--- test.py
+++
@@ -1,5 +0,0 @@
-def cars(company, **models):
-    print("The car brand is", company)
-    for key, value in models.items():
-        print(" ",key+" : ",value)
-cars("mclaren",model = "720s", horsepower = 720, color = "orange", price = "$650,000")
