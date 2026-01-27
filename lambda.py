b448cb5aae2829c33b34fd5f8e96e8df1ec862a0
M Heera Shanker <heerashanker0214@gmail.com>
2026-01-27 12:38:46 +0530

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,9 @@
+# The lambda function is small anonymous function
+# syntax lambda argument: expression 
+# lambda can take any number of arguments but only 1 expression
+x = lambda a : a+15
+print(x(5))
+
+# giving 2 arguments 
+v = lambda a,b : a+b
+print(v(5,6))
