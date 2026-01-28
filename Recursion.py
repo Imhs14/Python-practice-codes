264d81b021014d0cdd500b2ba8be581588ee62c5
M Heera Shanker <heerashanker0214@gmail.com>
2026-01-28 09:29:39 +0530

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -10,12 +10,12 @@ def countdown(n):
 countdown(5)
 
 
-#Base Case and Recursive Case
+"""Base Case and Recursive Case
 Every recursive function must have two parts:
 
 A base case - A condition that stops the recursion
 A recursive case - The function calling itself with a modified argument
-Without a base case, the function would call itself forever, causing a stack overflow error.
+Without a base case, the function would call itself forever, causing a stack overflow error."""
 
 def factorial(n):
     if n == 0 or n ==1:
