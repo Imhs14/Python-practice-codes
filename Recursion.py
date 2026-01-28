561ef190c5f27d4c876547b54587e0f659017bef
M Heera Shanker <heerashanker0214@gmail.com>
2026-01-28 14:25:24 +0530

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -22,4 +22,14 @@ def factorial(n):
         return 1
     else:
         return n* factorial(n-1)
-print(factorial(5))            
+print(factorial(5))  
+
+# Fibonacci sequence using recursions
+
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+print(fibo(7))      
+#O/P: 13              
